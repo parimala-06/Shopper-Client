067d5dbbd6b530913d4d19936bb515556fca2948
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-03-06 21:40:41 +0530

    Added all the components to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import React from "react";
 import './App.css';
-import Navbar from './components/Navbar/Navbar.js'
+import Navbar from "./components/Navbar/Navbar.js";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Shop from './pages/Shop.js';
+import ShopCategory from './pages/ShopCategory.js';
+import Product from './pages/Product.js';
+import Cart from './pages/Cart.js';
+import LoginSignUp from './pages/LoginSignUp.js';
+import Footer from "./components/Footer/Footer.js";
+import men_banner from './components/Assets/banner_mens.png';
+import women_banner from './components/Assets/banner_women.png';
+import kids_banner from './components/Assets/banner_kids.png';
+
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Shop/>}/>
+        <Route path='/men' element={<ShopCategory banner = {men_banner} category="men"/>}/>
+        <Route path='/women' element={<ShopCategory banner = {women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner = {kids_banner} category="kid"/>}/>
+        <Route path="/product" element={<Product/>}>
+          <Route path=":productId" element={<Product/>}/>
+        </Route>
+        <Route path="/cart" element={<Cart/>}/>
+        <Route path="/login" element={<LoginSignUp/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>      
     </div>
   );
 }
