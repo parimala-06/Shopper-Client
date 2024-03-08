e0d338b3159fb36fb6f6a39fce1e8b6737d04dbd
ParimalaGanesh <parimalaganeshsp@gmail.com>
2024-03-08 22:10:16 +0530

    Added styling to terms and conditions

--- src/components/Terms/Terms.js
+++ src/components/Terms/Terms.js
@@ -7,7 +7,6 @@ const Terms = () => {
         <h1>Shopper: Terms and Conditions</h1>
     <br />
     <br />
-
         <p>
         Welcome to Shopper, an online garments shop. By accessing or using our website, you agree to comply with and be bound by the following terms and conditions. Please read these Terms of Service carefully before accessing or using our website.
     <br />
